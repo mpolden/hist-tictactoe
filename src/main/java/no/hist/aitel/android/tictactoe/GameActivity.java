5512203a23cf5da027efc20e3d6593bc54471891
Vegard <vegardoj@vegardoj-asus.(none)>
2011-04-26 14:03:57 +0200

    Moved touch handling to gameactivity

--- src/main/java/no/hist/aitel/android/tictactoe/GameActivity.java
+++ src/main/java/no/hist/aitel/android/tictactoe/GameActivity.java
@@ -3,6 +3,9 @@ package no.hist.aitel.android.tictactoe;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
@@ -17,6 +20,58 @@ public class GameActivity extends Activity {
         gameView.setFocusable(true);
         gameView.setFocusableInTouchMode(true);
         gameView.setCellListener(new MyCellListener());
+        gameView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    return true;
+                } else if (action == MotionEvent.ACTION_UP) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    int sxy = gameView.getSxy();
+                    x = (x - 0) / sxy;
+                    y = (y - 0) / sxy;
+                    Toast.makeText(getApplicationContext(), String.valueOf(x + " " + y), Toast.LENGTH_SHORT).show();
+                    if (gameView.isEnabled() && x >= 0 && x < gameView.getBoardSize() && y >= 0 & y < gameView.getBoardSize()) {
+                        int cell = x + gameView.getBoardSize() * y;
+                        GamePlayer state = cell == gameView.getSelectedCell() ? gameView.getSelectedValue() : gameView.getController().get(x, y);
+                        state = state == GamePlayer.EMPTY ? gameView.getCurrentPlayer() : GamePlayer.EMPTY;
+                        gameView.setSelectedCell(cell);
+                        gameView.setSelectedValue(state);
+                        if (gameView.getController().get(x, y) == GamePlayer.EMPTY) {
+                            setCell(x, y, state);
+                            if (gameView.getCurrentPlayer() == GamePlayer.PLAYER1) {
+                                gameView.setCurrentPlayer(GamePlayer.PLAYER2);
+                            } else {
+                                gameView.setCurrentPlayer(GamePlayer.PLAYER1);
+                            }
+                        }
+                        if (cellListener != null) {
+                            cellListener.onCellSelected();
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setCell(int x, int y, GamePlayer player) {
+        if(gameView.getController().put(x, y, player) == GameState.VALID_MOVE) {
+            GameState s = gameView.getController().getState();
+            switch (s) {
+                case WIN: {
+                    
+                }
+                case DRAW: {
+
+                }
+            }
+        }
+        gameView.invalidate();
     }
 
     private class MyCellListener implements GameView.ICellListener {
