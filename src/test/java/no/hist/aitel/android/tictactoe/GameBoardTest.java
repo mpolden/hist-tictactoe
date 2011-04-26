aaf06e0a0f4e3c9ae54f4a6067aeda139bc58883
Martin Polden <martin.polden@gmail.com>
2011-04-26 14:28:32 +0200

    Add more tests for GameBoard

--- src/test/java/no/hist/aitel/android/tictactoe/GameBoardTest.java
+++ src/test/java/no/hist/aitel/android/tictactoe/GameBoardTest.java
@@ -6,12 +6,13 @@ import static no.hist.aitel.android.tictactoe.GamePlayer.PLAYER1;
 import static no.hist.aitel.android.tictactoe.GamePlayer.PLAYER2;
 import static no.hist.aitel.android.tictactoe.GameState.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GameBoardTest {
 
     @Test
     public void testPut() {
-        GameBoard b = new GameBoard(3);
+        final GameBoard b = new GameBoard(3);
         b.setInRow(3);
         assertEquals(VALID_MOVE, b.put(0, 0, PLAYER1));
         assertEquals(PLAYER1, b.get(0, 0));
@@ -21,7 +22,7 @@ public class GameBoardTest {
 
     @Test
     public void testGame3x3() {
-        GameBoard b = new GameBoard(3);
+        final GameBoard b = new GameBoard(3);
         b.setInRow(3);
         assertEquals(VALID_MOVE, b.put(0, 0, PLAYER1));
         assertEquals(PLAYER1, b.get(0, 0));
@@ -38,5 +39,86 @@ public class GameBoardTest {
         assertEquals(VALID_MOVE, b.put(2, 0, PLAYER1));
         assertEquals(PLAYER1, b.get(2, 0));
         assertEquals(WIN, b.getState());
+    }
+
+    @Test
+    public void testGame4x4() {
+        final GameBoard b = new GameBoard(4);
+        b.setInRow(4);
+        assertEquals(VALID_MOVE, b.put(0, 1, PLAYER1));
+        assertEquals(PLAYER1, b.get(0, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(2, 0, PLAYER2));
+        assertEquals(PLAYER2, b.get(2, 0));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(1, 1, PLAYER1));
+        assertEquals(PLAYER1, b.get(1, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 0, PLAYER2));
+        assertEquals(PLAYER2, b.get(3, 0));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(2, 1, PLAYER1));
+        assertEquals(PLAYER1, b.get(2, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 2, PLAYER2));
+        assertEquals(PLAYER2, b.get(3, 2));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 1, PLAYER1));
+        assertEquals(PLAYER1, b.get(3, 1));
+        assertEquals(WIN, b.getState());
+    }
+
+    @Test
+    public void testGame5x5() {
+        final GameBoard b = new GameBoard(5);
+        b.setInRow(5);
+        assertEquals(VALID_MOVE, b.put(4, 0, PLAYER2));
+        assertEquals(PLAYER2, b.get(4, 0));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(2, 0, PLAYER1));
+        assertEquals(PLAYER1, b.get(2, 0));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(1, 1, PLAYER2));
+        assertEquals(PLAYER2, b.get(1, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 0, PLAYER1));
+        assertEquals(PLAYER1, b.get(3, 0));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(2, 1, PLAYER2));
+        assertEquals(PLAYER2, b.get(2, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 2, PLAYER1));
+        assertEquals(PLAYER1, b.get(3, 2));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(3, 1, PLAYER2));
+        assertEquals(PLAYER2, b.get(3, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(4, 2, PLAYER1));
+        assertEquals(PLAYER1, b.get(4, 2));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(0, 1, PLAYER2));
+        assertEquals(PLAYER2, b.get(0, 1));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(2, 2, PLAYER1));
+        assertEquals(PLAYER1, b.get(2, 2));
+        assertEquals(NEUTRAL, b.getState());
+        assertEquals(VALID_MOVE, b.put(4, 1, PLAYER2));
+        assertEquals(PLAYER2, b.get(4, 1));
+        System.out.println(b.toString());
+        assertEquals(WIN, b.getState());
+    }
+
+    @Test
+    public void testGame6x6() {
+        final GameBoard b = new GameBoard(5);
+        b.setInRow(5);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGame7x7() {
+        final GameBoard b = new GameBoard(5);
+        b.setInRow(5);
+        assertTrue(true);
     }
 }
