95eeddf048f26f9cac133152e9671329aa17bae1
Vegard <vegardoj@vegardoj-asus.(none)>
2011-04-27 11:21:38 +0200

    Updated the layout for joingame dialog. Added listeners to the buttons.

--- src/main/java/no/hist/aitel/android/tictactoe/GameMultiplayerActivity.java
+++ src/main/java/no/hist/aitel/android/tictactoe/GameMultiplayerActivity.java
@@ -6,12 +6,15 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameMultiplayerActivity extends Activity {
 
     private static final int JOIN_DIALOG_ID = 0;
     private static final int HOST_DIALOG_ID = 1;
     private static final int LISTENING_PORT = 8080;
+    private String join_ip;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,29 @@ public class GameMultiplayerActivity extends Activity {
         findViewById(R.id.button_join).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog(JOIN_DIALOG_ID);
+                final Dialog joinDialog = new Dialog(GameMultiplayerActivity.this);
+                joinDialog.setContentView(R.layout.joingamedialog);
+                joinDialog.setTitle("Join game");
+                joinDialog.setCancelable(true);
+
+                TextView tv_joingame = (TextView) joinDialog.findViewById(R.id.textview_joingame);
+                tv_joingame.setText(R.string.join_game_dialog);
+
+                Button button_ok = (Button) joinDialog.findViewById(R.id.button_joingame_ok);
+                button_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        joinDialog.dismiss();
+                    }
+                });
+                Button button_cancel = (Button) joinDialog.findViewById(R.id.button_joingame_cancel);
+                button_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        joinDialog.dismiss();
+                    }
+                });
+                joinDialog.show();
             }
         });
         findViewById(R.id.button_host).setOnClickListener(new View.OnClickListener() {
