3a4ac3c7f632e684bf3ca77d71ee10fb5fe51e6c
Vegard <vegardoj@vegardoj-asus.(none)>
2011-04-28 11:33:21 +0200

    postinavlidate on makeBoard

--- src/main/java/no/hist/aitel/android/tictactoe/GameView.java
+++ src/main/java/no/hist/aitel/android/tictactoe/GameView.java
@@ -44,7 +44,7 @@ public class GameView extends View {
     public void makeBoard(int boardSize, int inarow) {
         this.boardSize = boardSize;
         this.board = new GameBoard(boardSize, inarow);
-        this.invalidate();
+        this.postInvalidate();
     }
 
     public GameBoard getBoard() {
