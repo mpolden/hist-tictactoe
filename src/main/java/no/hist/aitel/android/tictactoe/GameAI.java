af24719bb718f969862f483f3d3206c5c2ea7769
Martin Polden <martin.polden@gmail.com>
2011-05-01 18:54:20 +0200

    Implement easy AI

--- src/main/java/no/hist/aitel/android/tictactoe/GameAI.java
+++ src/main/java/no/hist/aitel/android/tictactoe/GameAI.java
@@ -1,36 +1,34 @@
 package no.hist.aitel.android.tictactoe;
 
-
+import java.util.List;
 import java.util.Random;
 
 public class GameAI {
-    public static final int EASY = 0;
-    public static final int MEDIUM = 1;
-    public static final int HARD = 2;
 
-    private Random random = new Random();
-    private GameBoard gameBoard;
-    private int difficulty;
-    private int boardSize;
-    private int lengthToWin;
+    private static final int EASY = 0;
+    private static final int MEDIUM = 1;
+    private static final int HARD = 2;
+
+    private final Random random;
+    private final GameBoard gameBoard;
+    private final int difficulty;
 
     public GameAI(GameBoard gameBoard, int difficulty) {
+        this.random = new Random();
         this.gameBoard = gameBoard;
         this.difficulty = difficulty;
-        boardSize = gameBoard.getBoard().length;
-        lengthToWin = gameBoard.getLengthToWin();
     }
 
-    public int[] getMove() {
-        int[] move = new int[2];
+    public Position getMove() {
         switch (difficulty) {
             case EASY: {
-                
+                final List<Position> emptyPositions = gameBoard.findEmpty();
+                return emptyPositions.get(random.nextInt(emptyPositions.size()));
+            }
+            default: {
+                return null;
             }
         }
-        return move;
     }
-
-
 
 }
