4184dd8226e1a31799bb946bf1d3016f81cfd790
Martin Polden <martin.polden@gmail.com>
2011-05-01 18:46:38 +0200

    Add Position class

---
+++ src/main/java/no/hist/aitel/android/tictactoe/Position.java
@@ -0,0 +1,20 @@
+package no.hist.aitel.android.tictactoe;
+
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
