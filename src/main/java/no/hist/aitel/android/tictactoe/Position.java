c61f125a0441de071cd452e2654b9275450b35f0
Martin Polden <martin.polden@gmail.com>
2011-05-01 18:53:20 +0200

    Add equals and hashCode to Position

--- src/main/java/no/hist/aitel/android/tictactoe/Position.java
+++ src/main/java/no/hist/aitel/android/tictactoe/Position.java
@@ -17,4 +17,24 @@ public class Position {
     public int getY() {
         return y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        if (y != position.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
